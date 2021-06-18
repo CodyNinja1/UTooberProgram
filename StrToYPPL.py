09dfa70f05719630c0ca9406b78150889ee6b81b
codyninja1 <82186696+CodyNinja1@users.noreply.github.com>
2021-06-18 09:43:14 +0300

    Add files via upload

---
+++ StrToYPPL.py
@@ -0,0 +1,7 @@
+inp = input()
+out = ""
+for char in inp:
+    n = ord(char)
+    a = f"ADDTHISVIDEOTOYOURPLAYLIST {n}\nGOTOTHEDESCRIPTION\nLOOKATTHECARDINTHEUPPERRIGHTCORNER\n"
+    out = out + a
+print(out)
