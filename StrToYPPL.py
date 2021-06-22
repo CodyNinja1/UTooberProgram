2fff6deef82a6c444d83914308c7c3e852e93781
codyninja1 <82186696+CodyNinja1@users.noreply.github.com>
2021-06-22 15:14:19 +0300

    Update StrToYPPL.py

--- StrToYPPL.py
+++ StrToYPPL.py
@@ -4,4 +4,5 @@ for char in inp:
     n = ord(char)
     a = f"ADDTHISVIDEOTOYOURPLAYLIST {n}\nGOTOTHEDESCRIPTION\nLOOKATTHECARDINTHEUPPERRIGHTCORNER\n"
     out = out + a
+out += "EDITORNOTE This program was created using the StrToYPPL.py tool"
 print(out)
